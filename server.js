fea96926b29aa7b40c1f589df65c1e66243f391b
rjhern37 <rjhern37@neiu.edu>
2020-06-30 16:38:22 -0500

    adding finalized server files

--- server.js
+++ server.js
@@ -1,39 +1,3 @@
-// //*GET notes file
-// app.get("/notes", function(req, res) {
-//     res.sendFile(path.join(__dirname, "notes.html"));
-//   });
-
-
-// //GET * "index file"
-//   app.get("/*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "index.html"));
-//   });
-
-// // API routes
-//   app.get("/api/notes", function(req, res) {
-//     res.sendFile(path.join(__dirname, "view.html"));
-//   });
-// //POST to notes (USED from "Final Star Wars activity")
-//   app.post("/api/notes", function(req, res) {
-
-//     var newNote = req.body;
-
-//     // Using a RegEx Pattern to remove spaces from newCharacter
-//     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//     newNote.routeName = newNote.name.replace(/\s+/g, "");
-  
-//     console.log(newNote);
-  
-//     characters.push(newNote);
-  
-//     res.json(newNote);
-//   });
-
-//   app.delete("/api/notes/:id", function(req, res) {
-//     res.sendFile(path.join(__dirname, "view.html"));
-//   });
-
-
 // ==========================================
 // ==========================================
 // ==========================================
@@ -43,49 +7,96 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+var database = require("./db/db.json")
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+//Allows express to link to the public folder
+app.use(express.static(__dirname + "/public"));
+
+
+// Setting up Express to handle data parsing
+//Required for API calls
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//GET * "index file"
-  app.get("/*", function(req, res) {
+//GET index file
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
   });
 
-//*GET notes file
+//GET notes file
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
 
 // API routes
   app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.json(database);
   });
 
 //POST to notes (USED from "Final Star Wars activity")
   app.post("/api/notes", function(req, res) {
 
     var newNote = req.body;
+    var jsonFilePath = path.join(__dirname, "/db/db.json")
 
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newNote.routeName = newNote.name.replace(/\s+/g, "");
-  
-    console.log(newNote);
-  
-    characters.push(newNote);
-  
+   
+    let noteId = 100;
+
+    //Loop through the array to find the highest ID
+    for (let i = 0; i < database.length; i++){
+      let singleNote = database[i];
+
+      if (singleNote.id > noteId){
+        noteId = singleNote.id;
+      }
+    }
+
+    //Assinging an ID to the newNote
+    newNote.id = noteId + 1;
+    database.push(newNote)
+
+    //Rewriting DB.json file
+    fs.writeFile(jsonFilePath, JSON.stringify(database), function(err){
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Your note was saved!");
+
+    });
+
+    //Gives back the users new notes
     res.json(newNote);
+
   });
 
   app.delete("/api/notes/:id", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+    var jsonFilePath = path.join(__dirname, "/db/db.json");
+
+    for (let i = 0; i < database.length; i++){
+      //Looping through to find the note to delete via ID
+      if (database[i].id == req.params.id){
+        //Splicing the note out of the array 
+        database.splice(i, 1);
+        break;
+      }
+    }
+
+     //Rewriting DB.json file without the deleted note
+    fs.writeFile(jsonFilePath, JSON.stringify(database), function(err){
+      if(err){
+        return console.log(err);
+      } 
+      console.log("Your note has been deleted.");
+    });
+
+    //Gives back the users new notes without the deleted note
+    res.json(database);
+
   });
 
 
